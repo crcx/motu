b6e59e0178bd01905535ded45178155d12f2384a
Charles Childers <crc.platypus@gmail.com>
2009-01-18 11:20:20 -0500

    added an introduction text and some hints on using motu for new sessions

--- scripts/persist.js
+++ scripts/persist.js
@@ -9,17 +9,20 @@ function save()
   createCookie('motu', document.forms[0].tib.value, 'Sun, 17-Jan-2038 19:14:07 GMT');
 }
 
+function allnew()
+{
+  document.forms[0].tib.value  = 'Motu is a personal notepad and programming environment.\n\n';
+  document.forms[0].tib.value += "It's designed to allow short (~4k) notes to be written and edited, and to allow easy mixing of Forth code and comments. You can run the Forth code by highlighting it and clicking the Evaluate button on the right. The results of your code being run will display in the box below.\n\n";
+  document.forms[0].tib.value += 'If you run into problems, save your session and reload Motu. You can then reevaluate your code after fixing any bugs you encounter.\n';
+  document.forms[0].tib.value += '---------------------------------------------------------------------\n';
+  save();
+}
+
 function load()
 {
   var x = readCookie('motu')
   if (x)
     document.forms[0].tib.value = x;
   else
-    document.forms[0].tib.value = '';
-}
-
-function allnew()
-{
-  document.forms[0].tib.value = '';
-  save();
+    allnew();
 }
