589b5bf7c83600027d72a4c9963afe6317d25e2a
Charles Childers <crc.platypus@gmail.com>
2009-01-28 00:07:31 -0500

    oops; strip escaped quotes in save.php

--- ajax/save.php
+++ ajax/save.php
@@ -3,7 +3,7 @@ $data = $_POST['tib'];
 $file = "motu.txt";
 
 $fp = fopen($file, "w") or die("Couldn't open $file for writing!");
-fwrite($fp, $data) or die("Couldn't write values to file!");
+fwrite($fp, stripslashes($data)) or die("Couldn't write values to file!");
 
 fclose($fp);
 ?>
